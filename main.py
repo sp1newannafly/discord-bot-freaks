5841a2e4a81363d4dcd80ac5a1691a638a1ed2e5
Maks <95273230+sp1newannafly@users.noreply.github.com>
2023-04-14 02:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src import bot
 from dotenv import load_dotenv
 import sys
 import keep_alive
-
+keep_alive.keep_alive()
 def check_version() -> None:
     import pkg_resources
     import src.log
@@ -29,4 +29,4 @@ def check_version() -> None:
 if __name__ == '__main__': 
     check_version()
     bot.run_discord_bot()
-keep_alive.keep_alive()
+
